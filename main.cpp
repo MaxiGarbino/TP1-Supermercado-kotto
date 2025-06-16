1f1f334bb2efb39b2b7ff820351b2280904a78e1
Luciano Garbarino <lucigar01@gmail.com>
2025-06-16 11:47:23 -0300

    1-c y 3

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <direct.h>
-
+#include <cstring>
+#include <ctime>
 using namespace std;
 // template <typename Generico>
 
@@ -38,6 +39,12 @@ struct ListaCompras {
   short cantRequerida;
 };   
 
+struct RubroArt {
+    short codRubro;
+    int posicionArt;
+};
+
+
 void EmitirUnArray(int id[],int n);
 
 bool Abrir(Articulo Articulo[] ,DescripArt IndDescripArt[], Rubro Rubros[], ListaCompras ListaCompras[],int &nArticulos, int &nDescripArt, int &nRubros, int &nListaCompras);
@@ -46,23 +53,37 @@ bool LeerRubros(ifstream &arch, Rubro &rubro);
 bool LeerListaCompras(ifstream &arch, ListaCompras &compra);
 bool LeerDescripArts(ifstream &arch, DescripArt &desc);
 void ProcCompras(ListaCompras ListaCompras[], DescripArt IndDescripArt[], Articulo Articulos[], int nArticulos, int nDescripArt, int nRubros, int nListaCompras);
-
+void CrearTablaRubroArt(Articulo Articulos[], int nArticulos, RubroArt tablaRubroArt[], int &nRubroArt);
+void OrdxBur(RubroArt tabla[], int n);
+void IntCmb(RubroArt &a, RubroArt &b);
+long GetTime(int &hh, int &mm, int &ss);
+long GetDate(int &year, int &mes, int &dia, int &ds);
+void EmitirTicket(ListaCompras ListaCompras[], DescripArt IndDescripArt[], Articulo Articulos[], int nListaCompras, int nDescripArt);
 int main()   
 {   
-      Articulo Articulos[10000];
-      DescripArt IndDescripArt[10000];
-      Rubro Rubros[1000];
-      ListaCompras ListaCompras[10000];
-      int nArticulos = 0;
-      int nDescripArt = 0;
-      int nRubros = 0;
-      int nListaCompras = 0;
+    Articulo Articulos[10000];
+    DescripArt IndDescripArt[10000];
+    Rubro Rubros[1000];
+    ListaCompras ListaCompras[10000];
+    RubroArt tablaRubroArt[10000];
+    int nArticulos = 0;
+    int nDescripArt = 0;
+    int nRubros = 0;
+    int nListaCompras = 0;
+    int nRubroArt = 0;
       
-      Abrir (Articulos,IndDescripArt,Rubros,ListaCompras,nArticulos,nDescripArt,nRubros,nListaCompras);
+    Abrir (Articulos,IndDescripArt,Rubros,ListaCompras,nArticulos,nDescripArt,nRubros,nListaCompras);
+    CrearTablaRubroArt(Articulos, nArticulos, tablaRubroArt, nRubroArt);
+
 
     //   VolcarArchivos(/*lista de parámetros que correspondan*/); // indicados por el grupo de trabajo.
-    ProcCompras(ListaCompras,IndDescripArt,Articulos, nArticulos, nDescripArt, nRubros, nListaCompras);              
-    //   EmitirTicket(/*lista de parámetros que correspondan*/);
+    ProcCompras(ListaCompras,IndDescripArt,Articulos, nArticulos, nDescripArt, nRubros, nListaCompras);  
+    for (int i = 0; i < nListaCompras; i++)
+    {
+        cout << ListaCompras[i].descripcionArt << " " << ListaCompras[i].cantRequerida << endl;
+    }
+                 
+    EmitirTicket(ListaCompras,IndDescripArt,Articulos,nListaCompras,nDescripArt);
     //   EmitirArt_x_Rubro(/*lista de parámetros que correspondan*/);
     //   Cerrar (/*Articulos,IndDescripArt,Rubros,ListaCompras*/);
     //   return 0;
@@ -121,6 +142,10 @@ bool Abrir(Articulo Articulo[] ,DescripArt IndDescripArt[], Rubro Rubros[], List
         i++;
     }
     nListaCompras = i;
+    for (int i = 0; i < nListaCompras; i++)
+    {
+        cout <<"leer archivo "<< ListaCompras[i].descripcionArt << " " << ListaCompras[i].cantRequerida << endl;
+    }
     return true;
 }
 
@@ -129,7 +154,11 @@ void VolcarArchivos(Articulo Articulo[] ,DescripArt IndDescripArt[], Rubro Rubro
  }
 
  void ProcCompras(ListaCompras ListaCompras[], DescripArt IndDescripArt[], Articulo Articulos[], int nArticulos, int nDescripArt, int nRubros, int nListaCompras) {
-     for (int i = 0; i < nListaCompras; i++)
+    for (int i = 0; i < nListaCompras; i++)
+    {
+        cout << "ProcCompras " << ListaCompras[i].descripcionArt << " " << ListaCompras[i].cantRequerida << endl;
+    } 
+    for (int i = 0; i < nListaCompras; i++)
      {
       int j = 0;
         while (ListaCompras[i].descripcionArt != IndDescripArt[j].descripcionArt) {
@@ -223,6 +252,93 @@ bool LeerDescripArts(ifstream &arch, DescripArt &desc) {
     return true;
 }
 
+void CrearTablaRubroArt(Articulo Articulos[], int nArticulos, RubroArt tablaRubroArt[], int &nRubroArt) {
+    for (int i = 0; i < nArticulos; i++) {
+        tablaRubroArt[i].codRubro = Articulos[i].codRubro;
+        tablaRubroArt[i].posicionArt = i;
+    }
+    nRubroArt = nArticulos;
+    OrdxBur(tablaRubroArt, nRubroArt);
+}
+
+void OrdxBur(RubroArt tabla[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (tabla[j].codRubro > tabla[j + 1].codRubro) {
+                IntCmb(tabla[j], tabla[j + 1]);
+            }
+        }
+    }
+}
+
+void IntCmb(RubroArt &a, RubroArt &b) {
+    RubroArt aux = a;
+    a = b;
+    b = aux;
+}
+
+void EmitirTicket(ListaCompras ListaCompras[], DescripArt IndDescripArt[], Articulo Articulos[], int nListaCompras, int nDescripArt) {
+    
+    freopen("../Ticket.txt", "w", stdout); // Redirige cout al archivo
+
+    cout << "K O T T O\nYo te reconozco\nSUC 170\nXXXXXXXXX 9999\nC.U.I.T. 99-99999999-9\n";
+    cout << "Fecha: ";
+    int y, m, d, ds;
+    GetDate(y, m, d, ds);
+    cout << d << "/" << m << "/" << y << "\n";
+    int h, mi, s;
+    GetTime(h, mi, s);
+    cout << "Hora: " << h << ":" << mi << ":" << s << "\n";
+    cout << "Nro. Ticket: 0001-00000001\nNro. Caja: 0001\n";
+    cout << "----------------------------------------\nF A C T U R A - B\nORIGINAL\n----------------------------------------\n";
+
+    float totalSinDescuentos = 0, totalDescuentos = 0;
+
+    for (int i = 0; i < nListaCompras; i++) {
+        // Buscar artículo
+        for (int j = 0; j < nDescripArt; j++) {
+            if (strcmp(ListaCompras[i].descripcionArt, IndDescripArt[j].descripcionArt) == 0) {
+                if (IndDescripArt[j].estado == false || ListaCompras[i].cantRequerida == 0) continue;
+
+                Articulo &art = Articulos[IndDescripArt[j].posicionArt];
+                float subtotal = ListaCompras[i].cantRequerida * art.precioUnitario;
+                float descuento = 0.0;
+
+                // Supongamos: primer promoción válida si existe
+                if (art.porcOfertas[0] != 0) {
+                    descuento = subtotal * (art.porcOfertas[1] / 100.0f);
+                }
+
+                totalSinDescuentos += subtotal;
+                totalDescuentos += descuento;
+
+                // Cuerpo del ticket
+                cout << ListaCompras[i].cantRequerida << " x $ " << art.precioUnitario << endl;
+                cout << art.descripcionArt << " \t " << art.unidadMedida << endl;
+                cout << art.codArt << "\t\t\t\t$ " << subtotal << endl;
+                if (descuento > 0)
+                    cout << "Promo\t\t\t\t\t$ -" << descuento << endl;
+                cout << "\n";
+                break;
+            }
+        }
+    }
+
+    float totalFinal = totalSinDescuentos - totalDescuentos;
+
+    // Totales
+    cout << "SubTot. sin descuentos....:         $ " << totalSinDescuentos << endl;
+    cout << "Descuentos por promociones:         $ -" << totalDescuentos << endl;
+    cout << "========================================\n";
+    cout << "T O T A L                            $ " << totalFinal << endl;
+    cout << "========================================\n";
+    cout << "Su pago con Tipo Pago:              $ " << totalFinal << endl;
+    cout << "Su vuelto:                          $ 0.00\n";
+    cout << "G R A C I A S  P O R  S U  C O M P R A\n";
+    cout << "Para consultas: infoKotto.com.ar\n";
+
+    fclose(stdout); // Cierra la redirección de salida
+}
 
 // void CabeceraTicket(int &ds){
 
@@ -250,26 +366,26 @@ bool LeerDescripArts(ifstream &arch, DescripArt &desc) {
 // string Replicate(char car, unsigned n){
 
 // }
-// long GetDate(int &year, int &mes, int &dia, int &ds) {
-//   time_t     rawtime;
-//   struct tm *timeinfo;
+long GetDate(int &year, int &mes, int &dia, int &ds) {
+   time_t     rawtime;
+   struct tm *timeinfo;
 
-//   time ( &rawtime );
-//   timeinfo = localtime ( &rawtime );
-//   year = 1900 + timeinfo->tm_year;
-//   mes  = 1 + timeinfo->tm_mon;
-//   dia  = timeinfo->tm_mday;
-//   ds   = 1 + timeinfo->tm_wday;
-//   return (1900 + timeinfo->tm_year) * 10000 + (1 + timeinfo->tm_mon) * 100 + timeinfo->tm_mday;
-// }
-// long GetTime(int &hh, int &mm, int &ss) {
-//   time_t     rawtime;
-//   struct tm *timeinfo;
+   time ( &rawtime );
+   timeinfo = localtime ( &rawtime );
+   year = 1900 + timeinfo->tm_year;
+   mes  = 1 + timeinfo->tm_mon;
+   dia  = timeinfo->tm_mday;
+   ds   = 1 + timeinfo->tm_wday;
+   return (1900 + timeinfo->tm_year) * 10000 + (1 + timeinfo->tm_mon) * 100 + timeinfo->tm_mday;
+ }
+long GetTime(int &hh, int &mm, int &ss) {
+   time_t     rawtime;
+   struct tm *timeinfo;
 
-//   time ( &rawtime );
-//   timeinfo = localtime ( &rawtime );
-//   hh = timeinfo->tm_hour;
-//   mm = timeinfo->tm_min;
-//   ss = timeinfo->tm_sec;
-//   return timeinfo->tm_hour * 10000 + timeinfo->tm_min * 100 + timeinfo->tm_sec;
-// }
+   time ( &rawtime );
+   timeinfo = localtime ( &rawtime );
+   hh = timeinfo->tm_hour;
+   mm = timeinfo->tm_min;
+   ss = timeinfo->tm_sec;
+   return timeinfo->tm_hour * 10000 + timeinfo->tm_min * 100 + timeinfo->tm_sec;
+ }
